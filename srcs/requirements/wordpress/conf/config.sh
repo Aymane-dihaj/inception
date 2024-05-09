03a329ad7081414dcb9293381e21fb83b34f2166
nozel <adihaj.1337@gmail.com>
2024-05-09 22:48:50 +0100

    still working on wordpress

--- srcs/requirements/wordpress/conf/config.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-wp config create    --allow-root \
-                    --dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
-                    --dbhost=maria:3306 --path='/var/www/wordpress'
-                    
